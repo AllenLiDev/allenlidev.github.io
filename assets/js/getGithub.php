7fb87646947aafc6c7af93d9acce35a3b2241a0e
Ming Li <headhuntar@gmail.com>
2018-09-12 22:42:22 -0700

    added ability to get json from github of last commit, TO DO incorporate latest commit or last time of commit onto webpage

---
+++ assets/js/getGithub.php
@@ -0,0 +1,20 @@
+<?php 
+
+    // set username
+    $username = $_GET['username'];
+
+    // create curl resource 
+    $ch = curl_init();
+
+    // set url 
+    curl_setopt($ch, CURLOPT_URL, "https://api.github.com/users/headhuntar/events"); 
+
+    //return the transfer as a string 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+    // $output contains the output string 
+    $output = curl_exec($ch); 
+
+    // close curl resource to free up system resources 
+    curl_close($ch);      
+?>
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,29 @@
 $(document).ready(function () {
     AOS.init({ duration: 1000 });
+    getGithub('headhuntar');
 });
+
+let getGithub = (username) => {
+    $.ajax({
+        async: false,
+        type: "GET",
+        url: "https://api.github.com/users/" + username + "/events",
+        dataType: "json",
+        success: function(data) {
+            console.log(data);
+        }
+    });
+}
+
+// var xmlhttp = new XMLHttpRequest();
+// xmlhttp.onreadystatechange = function() {
+//   if (this.readyState == 4 && this.status == 200) {
+//     displayProjects(JSON.parse(this.responseText));
+//   }
+// };
+// xmlhttp.open('GET', 'api/projects');
+// xmlhttp.send();
+
+// https://api.github.com/users/headhuntar/events
+// git hub api to get user event
+
